4289076cc9086b43a667a784bf4d38a1735ede85
Aishwarya <aishwaryarao76@gmail.com>
2025-07-25 22:24:15 +0530

    code written for lambda deplyment

--- main.tf
+++ main.tf
@@ -7,3 +7,32 @@
 provider "aws" {
                 region = "us-east-1"
 }
+// Now ill go to lambda to create an automation script to call polly
+// after that i will need to deploy that lambda function using terraform for aws to septup lambda initially
+
+resource "aws_iam_role" "lambda_exec_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "text_to_speech" {
+  filename         = "lambda.zip"
+  function_name    = "textToSpeechLambda"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "handler.lambda_handler"
+  runtime          = "python3.12"
+  source_code_hash = filebase64sha256("lambda.zip")
+}
