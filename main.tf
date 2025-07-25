d67abca3f614f811a962a2066350e0265dd8653e
Aishwarya <aishwaryarao76@gmail.com>
2025-07-25 21:26:52 +0530

    main.tf created

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
 // hii im writing this because I'll be creating a terraform script to deploy aws polly .
+// hi i'm starting the project 
+//firstly i started with configuring aws cli by installing it and giving credentials of iam user ie access key, password, region and output format of the code for reading 
+// now I'm writing terraform main script that is used for invoking polly through lambda.
+//this script will automatically deploy lambda.
+
+provider "aws" {
+                region = "us-east-1"
+}
