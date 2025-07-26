0a1130f5e5522cc4a23a434c415d3e3f8fd9b0be
Aishwarya <aishwaryarao76@gmail.com>
2025-07-26 20:58:50 +0530

    output file created

--- output.tf
+++ output.tf
@@ -1,8 +1,9 @@
 // This file is required to display the result or the output after main.tf runs .
 // it will display the name of lambda function and the arn it is using
+
 output "function_name" {
-  value = aws_lambda_function.text_to_speech.function_name
+  value = aws_lambda_function.text_to_speech.function_name // will display function name
 }
 output "function_arn" {
-    value = aws_lambda_function.text_to_speech.arn
+    value = aws_lambda_function.text_to_speech.arn // will display arn used
 }
